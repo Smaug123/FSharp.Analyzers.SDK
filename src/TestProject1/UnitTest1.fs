16603d24c8584acaeabbdc68ea543e6841ac52c0
Smaug123 <patrick+github@patrickstevens.co.uk>
2024-02-10 16:57:47 +0000

    where are the attributes

--- src/TestProject1/UnitTest1.fs
+++ src/TestProject1/UnitTest1.fs
@@ -7,23 +7,68 @@ open System.Runtime.InteropServices
 open System.Threading
 open FSharp.Analyzers.SDK
 open FSharp.Analyzers.SDK.TASTCollecting
+open FSharp.Compiler.Symbols
 open FSharp.Compiler.Text
 open FSharp.Compiler.CodeAnalysis
 open Ionide.ProjInfo
 open Ionide.ProjInfo.Types
 open Microsoft.Extensions.Logging.Abstractions
 open Microsoft.Extensions.Logging
-open NUnit.Framework
 
-[<TestFixture>]
-module TestProject1 =
+module Program =
     let logger = NullLogger.Instance
     let fileName = "/Users/patrick/Documents/GitHub/WoofWare.Myriad/ConsumePlugin/JsonRecord.fs"
     let projectFileName = "/Users/patrick/Documents/GitHub/WoofWare.Myriad/ConsumePlugin/ConsumePlugin.fsproj"
     let cache = Dictionary<string, ProjectOptions>()
+    let membersToIgnore = set [ "CompareTo"; "GetHashCode"; "Equals" ]
 
-    [<Test>]
-    let foo () : unit =
+    let exprTypesToIgnore =
+        set [ "Microsoft.FSharp.Core.int"; "Microsoft.FSharp.Core.bool" ]
+
+    type ContentsVisit<'ret> =
+        abstract AtEntity : FSharpEntity -> 'ret list -> 'ret
+        abstract AtMember : FSharpMemberOrFunctionOrValue -> FSharpMemberOrFunctionOrValue list list -> FSharpExpr -> 'ret
+        abstract AtInitAction : FSharpExpr -> 'ret
+
+    type RecordSpec =
+        {
+            Name : string
+            Attributes : FSharpAttribute list
+            Fields : (string * FSharpType) list
+        }
+
+    let rec getRecords (decl : FSharpImplementationFileDeclaration) : RecordSpec list =
+        match decl with
+        | FSharpImplementationFileDeclaration.InitAction a -> failwith "unexpected"
+        | FSharpImplementationFileDeclaration.Entity (e, d) ->
+            if e.IsFSharpRecord then
+                {
+                    Name = e.FullName
+                    Attributes = e.Attributes |> Seq.toList
+                    Fields =
+                        e.FSharpFields
+                        |> Seq.map (fun f ->
+                            f.FullName, f.FieldType
+                        )
+                        |> Seq.toList
+                }
+                |> List.singleton
+            else
+                d
+                |> List.collect getRecords
+        | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue (value, args, body) ->
+            if
+                not value.IsCompilerGenerated
+                || not (Set.contains value.CompiledName membersToIgnore)
+                || not body.Type.IsAbbreviation
+                || not (Set.contains body.Type.BasicQualifiedName exprTypesToIgnore)
+            then
+                // Value to treat, but we don't care about values
+                []
+            else []
+
+    [<EntryPoint>]
+    let foo _ : int =
         let toolsPath = Init.init (DirectoryInfo Environment.CurrentDirectory) None
 
         let loadProject properties projPath =
@@ -53,17 +98,6 @@ module TestProject1 =
 
             fcsPo
 
-        let state = ResizeArray<range>()
-
-        let walker =
-            { new TypedTreeCollectorBase() with
-                override _.WalkCall _ m _ _ _ range =
-                    let name = String.Join(".", m.DeclaringEntity.Value.FullName, m.DisplayName)
-
-                    if name = "Microsoft.FSharp.Core.FSharpOption`1.Value" then
-                        state.Add range
-            }
-
         let sourceText = File.ReadAllText fileName |> SourceText.ofString
         let properties =
             [
@@ -82,6 +116,12 @@ module TestProject1 =
             | Ok results -> Utils.createContext checkProjectResults fileName sourceText results
 
         match ctx.TypedTree with
-        | None -> ()
-        | Some typedTree -> walkTast walker typedTree
+        | None -> failwith "no tree"
+        | Some typedTree ->
+
+        for r in typedTree.Declarations |> List.collect getRecords do
+            printfn "%+A" r
+
+        printfn "Done"
+        0
 
